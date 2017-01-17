e6e6dda9291c00f2b7fab4214a46e211f7efe5d1
z0s0xp <z0s0xp@gmail.com>
2017-01-16 19:37:00 -0600

    Update Lenguajes.java

--- lenguajes/src/lenguajes/Lenguajes.java
+++ lenguajes/src/lenguajes/Lenguajes.java
@@ -16,7 +16,7 @@ public class Lenguajes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo"+"Desde github ");
     }
     
 }
