74eed9a6f2b156a089a076d484ad4ee049513d51
z0s0xp <z0s0xp@DESKTOP-JDBTR1L>
2017-01-23 19:48:48 -0600

    abstraccion

---
+++ lenguajes/src/nomina1/AplicacionNomina.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nomina1;
+
+/**
+ *
+ * @author z0s0xp
+ */
+public class AplicacionNomina {
+        public static void main(String[] args){
+            int x=2;
+            Trabajador worker=new Trabajador();
+            worker.nombre="Pedro";
+            worker.paterno="Lopez";
+            worker.numeroSeguro=90;
+            if(worker.numeroSeguro<=90)worker.numeroSeguro=100;
+            System.out.println("El nombre es:\n"+worker.nombre);
+              System.out.println("El seguro es es:\n"+worker.numeroSeguro);
+        
+}
+    
+}
