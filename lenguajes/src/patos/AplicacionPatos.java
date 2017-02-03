8672fabbda6843eb9410d4309507130bd8a6fc5c
T-102 <T-102@PC191>
2017-02-02 18:46:46 -0600

    hola

---
+++ lenguajes/src/patos/AplicacionPatos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patos;
+
+/**
+ *
+ * @author T-102
+ */
+public class AplicacionPatos {
+    
+    public static void main(String[] args) {
+        PatoCanadiense pc1=new PatoCanadiense();
+        PatoCanadiense pc2=new PatoCanadiense();
+        System.out.println(PatoCanadiense.color);
+        System.out.println(PatoEuropeo.color);
+        System.out.println(PatoMexicano.color);
+    }
+}
