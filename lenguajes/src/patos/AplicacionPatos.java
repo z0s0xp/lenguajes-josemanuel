abd14eeb038ac583b82836644b99204c7d5b5e8c
z0s0xp <z0s0xp@DESKTOP-JDBTR1L>
2017-02-09 19:36:56 -0600

    abstraccion

--- lenguajes/src/patos/AplicacionPatos.java
+++ lenguajes/src/patos/AplicacionPatos.java
@@ -12,11 +12,41 @@ package patos;
 public class AplicacionPatos {
     
     public static void main(String[] args) {
-        PatoCanadiense pc1=new PatoCanadiense();
-        PatoCanadiense pc2=new PatoCanadiense();
-        System.out.println(PatoCanadiense.color);
-        System.out.println(PatoEuropeo.color);
-        System.out.println(PatoMexicano.color);
-        pc1.comer();
-    }
+     int valores []={5,-2,5};
+     System.out.println(valores [2]);//exepcion error examen
+     int []dos={23, -8};
+     int tres []=new int [3];
+    
+     tres [1]=-100;
+     tres [0]=4;
+     tres [2]=9;
+    
+     
+     System.out.println(tres[1]);
+      for(int i=0;i<3;i++){
+          System.out.println(tres[i]);
+        
+          
+          
+          
+      }
+          for(int i: tres){
+              System.out.println(i);
+              
+          }
+          String saludo[]={"hola","como","estas"};
+     for(String x:saludo){
+         System.out.println(x);
+     }
+  
+     ComportamientoVolar patos[]=new ComportamientoVolar[5];
+     patos[0]=new PatoMexicano();
+     patos[1]=new PatoMexicano();
+     patos[2]=new PatoEuropeo();
+     patos[3]=new PatoEuropeo();
+     patos[4]=new PatoCanadiense();
+     for(ComportamientoVolar p:patos){
+         p.volar();
+     }
+    } 
 }
